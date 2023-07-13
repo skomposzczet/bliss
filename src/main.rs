7e127930ab0522e3c3b9049d894ba49a85d68218
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-07-13 22:21:07 +0200

    add cli for pull settings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+#![allow(dead_code)]
+
+use std::io::Write;
+
+use clap::{Parser, Subcommand};
+use url::Url;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Pull {
+        #[arg(short, long, help="Source username or email")]
+        username: String,
+
+        #[arg(short, long, help="Source instance")]
+        instance: Url,
+    },
+}
+
+
+enum Origin {
+    Source,
+    Destination,
+}
+
+fn get_password(origin: Origin) -> String {
+    let key = match origin {
+        Origin::Source => "LEMMY_SRC_PW",
+        Origin::Destination => "LEMMY_DST_PW",
+    };
+
+    match std::env::var(key) {
+        Ok(pw) => pw,
+        Err(_) => {
+            print!("Password({}): ", key);
+            std::io::stdout().flush().unwrap();
+            let pw = rpassword::read_password().unwrap();
+            pw
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Pull { username, instance }) => {
+            let pw = get_password(Origin::Source);
+        },
+        None => {}
+    }
 }
