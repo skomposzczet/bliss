e20fb9b361cb9b42c7cb3b4565bc37c2b62d771c
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-07-22 12:03:44 +0200

    add id to community wrapper

--- src/profile/community.rs
+++ src/profile/community.rs
@@ -1,10 +1,11 @@
-use lemmy_api_common::{lemmy_db_schema::{newtypes::DbUrl, source::community}, lemmy_db_views_actor::structs::CommunityView};
+use lemmy_api_common::{lemmy_db_schema::{newtypes::{DbUrl, CommunityId}, source::community}, lemmy_db_views_actor::structs::CommunityView};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Community {
     pub name: String,
     pub actor: DbUrl,
+    pub id: CommunityId,
 }
 
 impl Community {
@@ -12,6 +13,7 @@ impl Community {
         Community {
             name: community.name.to_owned(),
             actor: community.actor_id.clone(),
+            id: community.id,
         }
 
     }
@@ -21,6 +23,18 @@ impl Community {
             return false;
         }
         if self.actor != community.community.actor_id {
+            return false;
+        }
+        return true;
+    }
+}
+
+impl PartialEq for Community {
+    fn eq(&self, other: &Self) -> bool {
+        if self.name != other.name {
+            return false;
+        }
+        if self.actor != other.actor {
             return false;
         }
         return true;
